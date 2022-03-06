585e8998eab3a7f1ad5ae4b64e2eab83fcea36df
Cole Stoltz <96390471+ColeStolt@users.noreply.github.com>
2022-03-06 02:13:56 -0600

    Create CustomFont.java

---
+++ DataObjects/CustomFont.java
@@ -0,0 +1,11 @@
+package DataObjects;
+
+import java.awt.Font;
+
+public class CustomFont extends Font{
+
+    public CustomFont(int fontSize) {
+        super("Verdana", Font.PLAIN, fontSize);
+    }
+    
+}
