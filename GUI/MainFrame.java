74012ab4a40cac20a946226dd32345237552b0e9
Cole Stoltz <96390471+ColeStolt@users.noreply.github.com>
2022-03-06 01:59:07 -0600

    Create MainFrame.java

---
+++ GUI/MainFrame.java
@@ -0,0 +1,23 @@
+package GUI;
+
+/* This will hold the 3 Jpanels that make up the program:
+    Client View
+        - Shows a list of clients
+        - Each item is interactable
+        - More functions will come
+    Data Entry
+        - Where data entry happens
+        - New clients created
+        - New clients stored
+        - Contains a child tab
+        - Must select a client
+        - Child added to that client
+    GeneralInfo
+        - Contains date
+        - Contains time
+        - Contains user name
+*/
+
+public class MainFrame {
+    
+}
