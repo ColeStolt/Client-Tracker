0ba0efd1b807a5ea4eae8ddfd1e54e7514e7325d
Cole Stoltz <96390471+ColeStolt@users.noreply.github.com>
2022-03-07 00:29:11 -0600

    Update CustomFont.java
    Added description of the object

--- DataObjects/CustomFont.java
+++ DataObjects/CustomFont.java
@@ -2,6 +2,13 @@ package DataObjects;
 
 import java.awt.Font;
 
+/* Simple custom font. Makes life easier
+    and prevents the need for creating 
+    multiple different font objects in
+    different classes and re-filling 
+    the same data.
+*/
+
 public class CustomFont extends Font{
 
     public CustomFont(int fontSize) {
