e40d9f0a636696f395ba23cedba3dd8804c99562
Cole Stoltz <96390471+ColeStolt@users.noreply.github.com>
2022-04-19 20:11:45 -0500

    Update PgmStart.java

--- Main/PgmStart.java
+++ Main/PgmStart.java
@@ -1,22 +1,16 @@
 package Main;
 
+import javax.swing.JOptionPane;
+
 import AlgorithmsAndDataStructures.DataParser;
 import DataObjects.Child;
 import DataObjects.Client;
 import GUI.LoginScreen;
 
 public class PgmStart{
-    public static void main(String args[]){
-
-        //Client client = new Client();
-        
-
-        //client.addChild(new Child("Chocolate man", "10/14/2000", 12));
-        //client.removeChild("null");
-
-        //client.removeChild("lemonade");
+    public static void main(String args[]){       
 
         LoginScreen log = new LoginScreen();
-
+    
     }
 }
